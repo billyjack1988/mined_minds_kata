e39f76db1f56aa13aebcba2dfcf53e45a82bfa2a
billyjack1988 <billyjacktattoos@gmail.com>
2017-06-22 11:58:09 -0500

    3sand5s

--- mined=3minds=5.rb
+++ mined=3minds=5.rb
@@ -2,9 +2,11 @@ n =0
 100.times do 
 	n += 1
 	if n % 3 == 0
-		p "mined"
+		p "Mined"
 	elsif n % 5 == 0 
-		p "minds"
+		p "Minds"
+	elsif n % (3 - 5) == 0
+		p "MinedMinds"
 	else 
 		p n
   end 
